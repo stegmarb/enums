624a038c64e5b2bb4366aaf04468aa84bb08331e
Balazs Stegmar <stegmarb@gmail.com>
2018-08-20 19:53:55 +0200

    code for effective java presentation

---
+++ src/AfterEnums.java
@@ -0,0 +1,10 @@
+public class AfterEnums {
+
+    public static enum Apple{
+        FUJI, PIPPIN, GRANNY_SMITH
+    }
+
+    public static enum Orange{
+        NAVEL, TEMPLE, BLOOD
+    }
+}
---
+++ src/TryOut.java
@@ -0,0 +1,32 @@
+import java.util.Optional;
+
+public class TryOut {
+    public static void main(String[] args) {
+
+//        OperationSwitch.DIVIDE.apply(12, 12);
+//
+//        OperationApply.PLUS.apply(12, 12);
+
+//        double earthWeight = 82d;
+//        double mass = earthWeight / Planet.EARTH.surfaceGravity();
+//        for (Planet p : Planet.values())
+//            System.out.printf("Weight on %s is %f%n",
+//                    p, p.surfaceWeight(mass));
+
+//        System.out.println(OperationCSD.valueOf("PLUS"));
+
+//        Optional<OperationCSD> substraction = OperationCSD.fromString("-");
+//        System.out.println(substraction.toString());
+
+//        double x = 2d;
+//        double y = 4d;
+//        for (OperationCSD op : OperationCSD.values())
+//            System.out.printf("%f %s %f = %f%n",
+//                    x, op, y, op.apply(x, y));
+
+
+
+//        System.out.println(Ensemble.SOLO.numberOfMusicians());
+
+    }
+}
